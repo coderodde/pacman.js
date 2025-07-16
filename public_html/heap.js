a9b8b066cf05b5afcf16731e8e1e1b28b533c209
Rodion Efremov <coderodd3@yandex.com>
2025-07-16 18:11:15 +0300

    ...

--- public_html/heap.js
+++ public_html/heap.js
@@ -1,96 +1,165 @@
 var ds = {
-    Heap: function(eq, hash) {
-        this.size = 0;
+    HeapNode: function(element, priority, index) {
+        this.element = element;
+        this.priority = priority;
+        this.index = index;
+    },
+    
+    Heap: function(hashFunction) {
+        this.size  = 0;
         this.array = [];
-        this.eq = eq;
-        this.hash = hash;
-        this.indexMap = {};
+        this.map   = {};
+        this.hashFunction = hashFunction;
     }
 };
 
-ds.Heap.prototype.siftUp = function(i) {
-    if (i <= 0) {
+function getParentNodeIndex(index) {
+    return (index - 1) >>> 1;
+}
+
+ds.Heap.prototype.siftUp = function(index) {
+    if (index <= 0) {
         return;
     }
 
-    var p = (i - 1) >>> 1;
-    var element = this.array[i];
-
-    while (i > 0 && this.array[p].priority > element.priority) {
-        this.array[i] = this.array[p];
-        i = p;
-        p = (i - 1) >>> 1;
+    const targetNode = this.array[index];
+    const targetNodePriority = targetNode.priority;
+    var parentNodeIndex = getParentNodeIndex(index);
+    
+    while (true) {
+        const parentNode = this.array[parentNodeIndex];
+        const parentNodePriority = parentNode.priority;
+        
+        if (targetNodePriority < parentNodePriority) {
+            this.array[index] = parentNode;
+            parentNode.index = index;
+            index = parentNodeIndex;  
+            parentNodeIndex = getParentNodeIndex(index);
+        } else {
+            break;
+        }
+        
+        if (index === 0) {
+            break;
+        }
     }
-
-    this.indexMap[this.hash(element)] = i;
-    this.array[i] = element;
+    
+    this.array[index] = targetNode;
+    targetNode.index = index;
 };
 
-ds.Heap.prototype.siftDown = function(i) {
-    var minIndex = i;
-    var node = this.array[i];
+function getLeftChildIndex(index) {
+    return (index << 1) + 1;
+}
 
-    for (;;) {
-        var l = (i << 1) + 1;
-        var r = l + 1;
+function getRightChildIndex(index) {
+    return getLeftChildIndex(index) + 1;
+}
 
-        if (l < this.size && this.array[l].priority < node.priority) {
-            minIndex = l;
-        }
-
-        if (r < this.size && this.array[r].priority < Math.min(this.array[l].priority, node.priority)) {
-            minIndex = r;
-        }
-
-        if (minIndex === i) {
-            this.array[i] = node;
-            this.indexMap[this.hash(element)] = i;
-            return;
+ds.Heap.prototype.siftDown = function(index) {
+    
+    var leftChildNodeIndex  = getLeftChildIndex(index);
+    var rightChildNodeIndex = getRightChildIndex(index);
+    var minChildNodeIndex   = index;
+    
+    const targetHeapNode = this.array[index];
+    var leftChildNode = null;
+    
+    while (true) {
+        if (leftChildNodeIndex < this.size) {
+            leftChildNode = this.array[leftChildNodeIndex];
+            
+            if (leftChildNode.priority < targetHeapNode.priority) {
+                minChildNodeIndex = leftChildNodeIndex;
+            }
         } else {
-            this.array[i] = this.array[minIndex];
-            i = minIndex;
+            this.array[minChildNodeIndex] = targetHeapNode;
+            targetHeapNode.index = minChildNodeIndex;
+            return;
         }
+        
+        if (minChildNodeIndex === index) {
+            if (rightChildNodeIndex < this.size) {
+                const rightChildNode = this.array[rightChildNodeIndex];
+                
+                if (rightChildNode.priority < targetHeapNode.priority) {
+                    minChildNodeIndex = rightChildNodeIndex;
+                }
+            }
+        } else {
+            if (rightChildNodeIndex < this.size) {
+                const rightChildNode = this.array[rightChildNodeIndex];
+                
+                if (rightChildNode.priority < leftChildNode.priority) {
+                    minChildNodeIndex = rightChildNodeIndex;
+                }
+            }
+        }
+        
+        if (minChildNodeIndex === index) {
+            this.array[minChildNodeIndex] = targetHeapNode;
+            targetHeapNode.index = minChildNodeIndex;
+            return;
+        }
+        
+        this.array[index] = this.array[minChildNodeIndex];
+        this.array[index].index = index;
+        
+        index = minChildNodeIndex;
+        leftChildNodeIndex  = getLeftChildIndex (index);
+        rightChildNodeIndex = getRightChildIndex(index); 
     }
 };
 
 ds.Heap.prototype.add = function(element, priority) {
-    this.array.push({element: element, priority: priority});
-    this.siftUp(this.size);
-    this.indexMap[this.hash(element)] = this.size++;
+    const mapKey = this.hashFunction(element);
+    
+    if (this.map[mapKey]) {
+        // Already present, exit:
+        return;
+    }
+    
+    const node = new ds.HeapNode(element, 
+                                 priority, 
+                                 this.size);
+                                 
+    this.array.push(node);
+    this.siftUp(this.size++);
+    this.map[mapKey] = node;
 };
 
 ds.Heap.prototype.extractMinimum = function() {
     if (this.size <= 0) {
-        return undefined;
+        throw "Extracting from an empty heap!";
     }
 
-    var node = this.array[0];
+    var topNode = this.array[0];
     this.array[0] = this.array[--this.size];
+    this.siftDown(0);
+    this.array[this.size] = null;
     this.array.pop();
-
-    if (this.size > 0) {
-        this.siftDown(0);
-    }
-
-    return node.element;
+    const mapKey = this.hashFunction(topNode.element);
+    delete this.map[mapKey];
+    return topNode.element;
 };
 
-ds.Heap.prototype.changePriority = function(element, priority) {
-    var index = this.hash(element);
+ds.Heap.prototype.decreasePriority = function(element,
+                                              priority) {
+    const mapKey = this.hashFunction(element);
+    const node = this.map[mapKey];
     
-    if (!index) {
+    if (!node) {
+        // 'element', no such datum in this heap!
         return false;
     }
-
-    var node = this.array[index];
-
-    if (priority < node.priority) {
-        node.priority = priority;
-        this.siftUp(index);
-    } else if (priority > node.priority) {
-        node.priority = priority;
-        this.siftDown(index);
+    
+    if (priority >= node.priority) {
+        // Cannot lower the priority:
+        return false;
     }
+    
+    node.priority = priority;
+    this.siftUp(node.index);
 
     return true;
 };
